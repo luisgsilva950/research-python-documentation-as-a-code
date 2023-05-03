c658fe2b1f57fb95b8094c89d37df168cec021ee
luisdomingues <luisgsilva950@gmail.com>
2023-05-03 20:14:59 -0300

    fix label

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def example_state_machine():
     states = ['Home', 'Wait', 'Opponent Seek', 'Move Forward', 'Move Back']
 
     transitions = [StateMachineTransition(label='Turn On', source=states[0], destiny=states[1]),
-                   StateMachineTransition(label='5s', source=states[1], destiny=states[1]),
+                   StateMachineTransition(label='T < 5s', source=states[1], destiny=states[1]),
                    StateMachineTransition(label='T > 5s', source=states[1], destiny=states[2]),
                    StateMachineTransition(label='Opponent not found', source=states[2], destiny=states[2]),
                    StateMachineTransition(label='Opponent found', source=states[2], destiny=states[3]),
                    StateMachineTransition(label='Edge not found', source=states[3], destiny=states[3]),
                    StateMachineTransition(label='Edge found', source=states[3], destiny=states[4]),
-                   StateMachineTransition(label='100ms', source=states[4], destiny=states[4]),
+                   StateMachineTransition(label='T < 100ms', source=states[4], destiny=states[4]),
                    StateMachineTransition(label='T > 100ms', source=states[4], destiny=states[2])]
     return StateMachine(title="Simple Sumo Robot", states=states, transitions=[*transitions])
 
